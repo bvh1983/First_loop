d2de0afd23c14a1be6dc9fb955ced1adce2c1a0f
bvh1983 <vanhecke_bart@telenet.be>
2026-01-16 03:26:39 +0000

    first loops.py added

---
+++ First_loops.py
@@ -0,0 +1,29 @@
+###   imports
+import os
+import sys
+import time
+
+###   variables
+aantal=10
+nr=500
+waarde=0
+totaal=0
+
+###   functies
+#   toon menu
+def menushow():
+    while True:
+            
+#   berekening
+def berekening():
+    for i in range (aantal):
+        nr+=waarde
+
+
+###   programma 
+
+
+
+
+
+                        
